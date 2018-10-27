828e121ca8092eaf958ffd9fa898a9e1c95b38f7
Kacper <kacperlodzinski99@gmail.com>
2018-10-27 21:20:34 +0100

    change 2

---
+++ js/input.js
@@ -0,0 +1,23 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyC56DeZIqnGDiUpOtvL9_lcToRaItd8014",
+    authDomain: "reuniteme-662eb.firebaseapp.com",
+    databaseURL: "https://reuniteme-662eb.firebaseio.com",
+    projectId: "reuniteme-662eb",
+    storageBucket: "reuniteme-662eb.appspot.com",
+    messagingSenderId: "289356166789"
+};
+firebase.initializeApp(config);
+
+     
+    
+      var rootRef = firebase.database().ref().child('posts');
+      $('#submit').click(function(){
+          rootRef.push({
+                email:$('#email').val(),
+                description:$('#description').val(),
+                location:$('#location').val(),
+                date:$('#date').val()
+                
+          });
+      })
