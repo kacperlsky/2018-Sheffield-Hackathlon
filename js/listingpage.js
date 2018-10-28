d164197b8eeacc6aa7f9dfe51dac63eddba90ad5
Kacper <kacperlodzinski99@gmail.com>
2018-10-28 01:23:59 +0000

    change6.1
    changing the orange button send

--- js/listingpage.js
+++ js/listingpage.js
@@ -17,5 +17,5 @@ rootRef.on("child_added", snap => {
 	var description = snap.child('description').val();
 	var date = snap.child('date').val();
 	var lof = snap.child('lof').val();
-	$('#posts').append("<dl><dt id='"+ lof+ "'>" + lof + "<dt><dt>Location</dt><dd>"+ location+"</dd><dt>Date</dt><dd>"+date+"</dd><div id='circle' class='button'><img src='img/send_icon.svg' alt='Sendyour email' height='24' width='24'></div></dl>");
+	$('#posts').append("<dl><dt id='"+ lof+ "'>" + lof + "<dt><dt>Location</dt><dd>"+ location+"</dd><dt>Date</dt><dd>"+date+"</dd><img src='img/send_icon_orange.svg' alt='Sendyour email' height='24' width='24'></div></dl>");
 });
