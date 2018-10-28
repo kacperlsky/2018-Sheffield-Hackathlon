9ccdd28cdde8e3aa32742f28aa6efa96dafc4ab0
unknown <>
2018-10-28 06:13:56 +0000

    Embed image in posts - experimental

--- js/listingpage.js
+++ js/listingpage.js
@@ -3,10 +3,11 @@
 var rootRef = firebase.database().ref().child('posts');
 
 rootRef.on("child_added", snap => {
-	var email = snap.child('email').val();
-	var location = snap.child('location').val();
-	var description = snap.child('description').val();
-	var date = snap.child('date').val();
 	var lof = snap.child('lof').val();
-	$('#posts').append("<article><p class='"+ lof+ "'>" + lof + "</p><p><div class='name'>Description</div><div class='value'>"+ description +"</div></p><p><div class='name'>Location</div><div class='value'>"+ location+"</div></p><p><div class='name'>Date</div><div class='value'>"+date+"</div></p><p><img src='img/send_icon_orange.svg' alt='Send your email' height='24' width='24'></p></article>");
+	var email = snap.child('email').val();
+	var description = snap.child('description').val();
+	var location = snap.child('location').val();
+	var date = snap.child('date').val();
+	var url = snap.child('url').val() || "";
+	$('#posts').append("<article><p class='"+ lof+ "'>" + lof + "</p><p><div class='name'>Description</div><div class='value'>"+ description +"</div></p><p><div class='name'>Location</div><div class='value'>"+ location+"</div></p><p><div class='name'>Date</div><div class='value'>"+date+"</div></p><p><img src='"+url+"'><br/><img src='img/send_icon_orange.svg' alt='Send your email' height='24' width='24'></p></article>");
 });
