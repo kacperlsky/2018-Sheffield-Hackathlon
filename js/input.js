1ea7ab6addf1b06f994f2a0f942df76114e1e22e
Kacper <kacperlodzinski99@gmail.com>
2018-10-28 03:29:13 +0000

    change 7
    working on file uploading

--- js/input.js
+++ js/input.js
@@ -1,6 +1,38 @@
 
-     
-    
+     var uploader = document.getElementById('uploader');
+     var fileButton = document.getElementById('fileButton');
+
+     fileButton.addEventListener('change', function(e) {
+            //Get file
+            var file = e.target.files[0];
+
+            //Create a storage ref
+            var storageRef = firebase.storage().ref('images/' + file.name);
+
+            //Upload file
+            var task = storageRef.put(file);
+
+            task.on('state_changed', 
+            
+                function progress(snapshot)  {
+                    var percentage = (snapshot.bytesTransferred /
+                        snapshot.totalBytes) * 100;
+                        uploader.value = percentage;
+                },
+
+                function error(err){
+
+                },
+                
+                function complete(){
+
+                    
+                }
+            
+            );
+     });
+      
+      
       var rootRef = firebase.database().ref().child('posts');
       $('#submit_createnewpost').click(function(){
           rootRef.push({
