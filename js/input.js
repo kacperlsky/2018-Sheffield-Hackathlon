6b03176af64802d503cc6cb77dadf0270d4f9a31
Kacper <kacperlodzinski99@gmail.com>
2018-10-28 01:45:03 +0000

    change 6.4

--- js/input.js
+++ js/input.js
@@ -14,11 +14,11 @@ firebase.initializeApp(config);
       var rootRef = firebase.database().ref().child('posts');
       $('#submit_createnewpost').click(function(){
           rootRef.push({
-                lof:$('input[name=lof]:checked').val(),
-                email:$('#email').val(),
-                description:$('#description').val(),
-                location:$('#location').val(),
-                date:$('#date').val()
+                lof:$('input[name=lof_post]:checked').val(),
+                email:$('#email_post').val(),
+                description:$('#description_post').val(),
+                location:$('#location_post').val(),
+                date:$('#date_post').val()
                 
           });
       })
