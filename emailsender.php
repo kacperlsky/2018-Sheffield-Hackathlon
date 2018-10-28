6c9190f385a9f36ac214e998a8cb95deec7cf81d
Kacper <kacperlodzinski99@gmail.com>
2018-10-28 01:44:05 +0100

    change 6
    working on email sender

--- emailsender.php
+++ emailsender.php
@@ -1,8 +1,20 @@
 <?php
-$to = "moreni123@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: kacperlodzinski99@gmail.com" . "\r\n" ;
-
-mail($to,$subject,$txt,$headers);
+	}
+	else
+	{
+		ini_set('SMTP',$_POST['Host']);
+		$to = $_POST['kacperlodzinski99@gmail.com'];
+		$from = 'From: ' . $_POST['moreni123@gmail.com'];
+		$subject = $_POST['Subject'];
+		$message = $_POST['Message'];
+	
+		if(mail($to,$subject,$message,$from))
+		{
+			echo "Message Sent";
+		}
+		else
+		{
+			 echo "Message Not Sent";
+		}
+	}
 ?>
