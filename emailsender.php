f71e0b21add09ff58d2209bdc9c5f74671852cc0
Kacper <kacperlodzinski99@gmail.com>
2018-10-28 01:57:00 +0100

    Update emailsender.php

--- emailsender.php
+++ emailsender.php
@@ -1,7 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mail()</title>
+</head>
+<body>
 <?php
+	if (!array_key_exists('Submitted',$_POST))
+	{
+?>
+		<form method="post" action="Mail.php">
+		<input type="hidden" name="Submitted" value="true">
+		Mail Server: <input type="text" name="Host" size="25"><br>
+		To: <input type="text" name="To" size="25"><br>
+		From: <input type="text" name="From" size="25"><br>
+		Subject: <input type="text" name="Subject" size="25"><br>
+		<textarea name="Message" cols="50" rows="10"></textarea><br>
+		<input type="submit" value="Send Email">
+		</form>
+<?php
+	}
+	else
+	{
 		ini_set('SMTP',$_POST['Host']);
-		$to = $_POST['kacperlodzinski99@gmail.com'];
-		$from = 'From: ' . $_POST['moreni123@gmail.com'];
+		$to = $_POST['To'];
+		$from = 'From: ' . $_POST['From'];
 		$subject = $_POST['Subject'];
 		$message = $_POST['Message'];
 	
@@ -13,4 +36,7 @@
 		{
 			 echo "Message Not Sent";
 		}
+	}
 ?>
+</body>
+</html>
