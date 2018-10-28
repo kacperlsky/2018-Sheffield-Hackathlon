6ec7b4555366dc8b12e471d02ccd3c30a96a31cb
Kacper <kacperlodzinski99@gmail.com>
2018-10-28 02:39:35 +0000

    6.6
    everything should work well right now

--- js/input.js
+++ js/input.js
@@ -1,9 +1,9 @@
 
      
     
-      var rootRef1 = firebase.database().ref().child('posts');
+      var rootRef = firebase.database().ref().child('posts');
       $('#submit_createnewpost').click(function(){
-          rootRef1.push({
+          rootRef.push({
                 lof:$('input[name=lof_post]:checked').val(),
                 email:$('#email_post').val(),
                 description:$('#description_post').val(),
